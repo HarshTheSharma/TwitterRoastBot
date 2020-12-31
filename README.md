aa5139995a70af98e87c9f98fb18e7cee7c8f808
HarshTheSharma <71364679+HarshTheSharma@users.noreply.github.com>
2020-12-30 16:59:47 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+This program tweets a line from Depression.txt every 24 hours. 
 This script requires Twitter Developer access, namely an set OAUTH Keys and API Keys with Read and Write Access.
-Install Dependencies By `Pip install -r Requirements.txt`
+requires python 3.8+ and pip  
+install dependencies by `pip install -r requirements.txt`
--- ReplyToTweetsWithDepression.py
+++ ReplyToTweetsWithDepression.py
@@ -1,27 +1,20 @@
 #Copyright 2021 HarshTheSharma
-
-#imports
+#Imports
 import tweepy
 from datetime import datetime
 import time
 
-#variables
-tweet = " "
-currentline = 0
-auth = tweepy.OAuthHandler("API Key", "Secret API Key")
-auth.set_access_token("OAUTH Key", "Private OAUTH Key")
+#Initialize
+auth = tweepy.OAuthHandler("SCjKQj1bEBRjf57hTMZ64kfhK", "RbkC3za5dxbPAhiDuUImWVWamQZjajaOe6WHkNI5o4zX7V5Vr7")
+auth.set_access_token("1239000319683465216-PydYhld8VSZr9j7uAgBxrZzDz3ZRUM", "Xzu3CsYE5GndurmlfqEUW1JfAfBThM2XiiNifo1W48qWR")
 api = tweepy.API(auth)
 tweetlist = open('Depression.txt')
 
-#loop
+#Loop
 while True:
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     if current_time == "12:00":
-        lines = tweetlist.readlines()
-        tweet = lines[currentline]
-        api.update_status("@user " + tweet)
-        currentline = currentline + 1
-        if currentline == 51:
-            currentline = 1
-        time.sleep(60)
+        tweet = tweetlist.readline()
+        api.update_status("@HarshTheSharma " + tweet)
+        time.sleep(86400)
